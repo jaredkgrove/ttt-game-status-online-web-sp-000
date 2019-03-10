933c81833ec0856d9ac821bacb6b2ad175aa1cff
Jared Grove <jaredkgrove@gmail.com>
2019-03-10 17:59:29 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,6 +7,7 @@ end
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
   
   def won?(board)
+<<<<<<< HEAD
     WIN_COMBINATIONS.detect do |combination|
       combination.all? {|win_index| board[win_index] == "X"} || combination.all? {|win_index| board[win_index] == "O"}
     end
@@ -27,5 +28,12 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4
   def winner(board)
     if won?(board).class == Array
       board[won?(board)[0]]
+=======
+    WIN_COMBINATIONS.any? do |combination|
+      puts combination.all? {|win_index| position_taken?(board, win_index)} & " 1"
+      puts combination.all? {|win_index| board[win_index] == "X"}
+      puts combination.all? {|win_index| board[win_index] == "O"}
+      combination.all? {|win_index| position_taken?(board, win_index)} && (combination.all? {|win_index| board[win_index] == "X"} || combination.all? {|win_index| board[win_index] == "O"})
+>>>>>>> c2332910c21a626c6af9373ae2a53bea4680d1cd
     end
   end
